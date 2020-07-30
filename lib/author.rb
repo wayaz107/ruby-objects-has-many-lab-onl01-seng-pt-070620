ae443a47f64bff1f8cb35031c44a2a190cf49721
Warda Ayaz <wayaz107@gmail.com>
2020-07-30 06:01:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,27 @@
 class Author
 
+attr attr_accessor :name
+
+@@all = [] 
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+end 
+
+def add_post(post)
+  post.author = self
+end 
+
+def add_post_by_title(post_title)
+  post = Post.new(post_title)
+  post.author = self
+end 
+
+def self.post_count
+  Post.all.count
+end 
+
 
 
 end 
