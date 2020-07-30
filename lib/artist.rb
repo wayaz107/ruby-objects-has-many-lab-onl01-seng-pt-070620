ddec3f390243cbf65c892e17dc4adb9f50c97bff
Warda Ayaz <wayaz107@gmail.com>
2020-07-30 05:27:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
 attr_accessor :name, :songs
+
 @@song_count = 0
 
 def initialize(name)
@@ -15,6 +16,7 @@ end
 
 def add_song_by_name(name)
   song = Song.new(name)
+  @song << song
   song.artist = self
   @@song_count +=1
 end
